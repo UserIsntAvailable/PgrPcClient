d123998ee951ef4ce713852011f8f9dda5d2c7c6
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-08-05 11:21:54 -0400

    Add(Macros)

---
+++ Win32Api/Macros.cs
@@ -0,0 +1,9 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable IdentifierTypo
+namespace Win32Api
+{
+    public static class Macros
+    {
+        public static nint HIWORD(nint value) => value >> 16;
+    }
+}
