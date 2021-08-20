b158047b68654d9c098c57eceeedade229e287b9
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-08-19 20:21:20 -0400

    Add(IAdbClientExtensions)

---
+++ AdbMouseFaker/IAdbClientExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Net;
+using SharpAdbClient;
+
+namespace AdbMouseFaker
+{
+    public static class IAdbClientExtensions
+    {
+        public static DeviceData ConnectToDevice(this IAdbClient client, string deviceName, DnsEndPoint endPoint)
+        {
+            client.Connect(endPoint);
+            var devices = client.GetDevices();
+
+            return devices.First(n => n.Serial == deviceName);
+        }
+    }
+}
