d309fc54eb91fbb63473f84d24fc6793cf758390
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-09-12 18:55:40 -0400

    Change(Macros): Add 'RGB' macro.

--- Win32Api/Macros.cs
+++ Win32Api/Macros.cs
@@ -9,6 +9,8 @@ namespace Win32Api
 
         public static int GET_Y_LPARAM(int lp) => (short) HIWORD(lp);
 
+        public static ulong RGB(byte r, byte g, byte b) => (ulong) (r | (g << 8) | (int) ((ulong) b << 16));
+
         public static ushort HIWORD(nint value) => (ushort) (value >> 16);
 
         public static ushort LOWORD(nint value) => (ushort) (value & 0xffff);
