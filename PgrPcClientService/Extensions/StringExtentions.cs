38a7c82037236c571431bc6c6c43f0315a843c6b
Angel Pineda <pinedap.angel@gmail.com>
2021-09-14 10:37:08 -0400

    Change(StringExtensions): Make 'IsHexValue' case insensitive.

--- PgrPcClientService/Extensions/StringExtentions.cs
+++ PgrPcClientService/Extensions/StringExtentions.cs
@@ -1,7 +1,9 @@
+using System.Globalization;
+
 namespace PgrPcClientService.Extensions
 {
     public static class StringExtensions
     {
-        public static bool IsHexValue(this string str) => str.StartsWith("0x");
+        public static bool IsHexValue(this string str) => str.StartsWith("0x", true, CultureInfo.CurrentCulture);
     }
 }
