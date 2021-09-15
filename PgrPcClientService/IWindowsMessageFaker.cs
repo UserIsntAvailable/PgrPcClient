658f7524113a2a645722876d1becd36df7475699
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-09-15 01:18:37 -0400

    Add(I/WindowsMessageFaker): No implemented yet.

---
+++ PgrPcClientService/IWindowsMessageFaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static Win32Api.Message;
+
+namespace PgrPcClientService
+{
+    public interface IWindowsMessageFaker
+    {
+        public Func<nint,uint,nint,nint,nint> MessageSender { get; set; }
+        
+        public nint KeyMessage(WM wM, nint hWnd, nint wParam, nint lParam);
+
+        public nint VirtualKeyMessage(nint vK, nint hWnd, WM wM);
+    }
+}
---
+++ PgrPcClientService/WindowsMessageFaker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Win32Api;
+using static Win32Api.Message;
+
+namespace PgrPcClientService
+{
+    public class WindowsMessageFaker : IWindowsMessageFaker
+    {
+        private readonly IDictionary<nint, nint> _binds;
+
+        public WindowsMessageFaker(IDictionary<nint, nint> binds, Func<nint, uint, nint, nint, nint> messageSender)
+        {
+            _binds = binds;
+            this.MessageSender = messageSender;
+        }
+
+        public Func<nint, uint, nint, nint, nint> MessageSender { get; set; }
+
+        public nint KeyMessage(WM wM, nint hWnd, nint wParam, nint lParam) => throw new NotImplementedException();
+
+        public nint VirtualKeyMessage(nint vK, nint hWnd, Message.WM wM) => throw new NotImplementedException();
+    }
+}
