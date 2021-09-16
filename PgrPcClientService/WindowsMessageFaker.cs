f4fb57cad05154a222c26ecc0c7b8d11ab0e16e7
Angel Pineda <pinedap.angel@gmail.com>
2021-09-16 13:24:15 -0400

    Change(WindowsMessageFaker): Implement methods.

--- PgrPcClientService/WindowsMessageFaker.cs
+++ PgrPcClientService/WindowsMessageFaker.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Win32Api;
 using static Win32Api.Message;
+using static Win32Api.Keyboard;
 
 namespace PgrPcClientService
 {
@@ -9,6 +9,7 @@ namespace PgrPcClientService
     {
         private readonly nint _appHWnd;
         private readonly IDictionary<nint, nint> _binds;
+        private readonly nint _currentKeyboardLayout = GetKeyboardLayout(0);
 
         public WindowsMessageFaker(
             nint appHWnd,
@@ -22,8 +23,36 @@ namespace PgrPcClientService
 
         public Func<nint, uint, nint, nint, nint> MessageSender { get; set; }
 
-        public nint KeyMessage(WM wM, nint wParam, nint lParam) => throw new NotImplementedException();
+        public nint KeyMessage(WM wM, nint wParam, nint lParam)
+        {
+            var message = (uint) wM;
 
-        public nint VirtualKeyMessage(nint vK, WM wM) => throw new NotImplementedException();
+            return _binds.ContainsKey(wParam)
+                ? this.VirtualKeyMessage(wParam, wM)
+                : this.MessageSender(_appHWnd, message, wParam, lParam);
+        }
+
+        public nint VirtualKeyMessage(nint vK, WM wM)
+        {
+            var message = (uint) wM;
+
+            if(!_binds.TryGetValue(vK, out var value)) return 0;
+
+            var scanCode = MapVirtualKeyExA((uint) value, (uint) MAPVK.VK_TO_VSC, _currentKeyboardLayout);
+
+            var newLParam = FakeKeyLParam(scanCode, wM == WM.KEYDOWN);
+
+            return this.MessageSender(_appHWnd, message, value, newLParam);
+
+            static nint FakeKeyLParam(nint scanCode, bool isKeyDown)
+            {
+                const int repeat = 1;
+
+                var scancode = (int) scanCode << 16;
+                var downOrUpFlag = isKeyDown ? 0 : 3 << 30;
+
+                return repeat + scancode + downOrUpFlag;
+            }
+        }
     }
 }
