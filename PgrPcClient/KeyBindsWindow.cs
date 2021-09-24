df258e7a62d991a66346b582c27fb25d33c7191b
Angel Pineda <pinedap.angel@gmail.com>
2021-09-24 14:21:17 -0400

    Change(KeyBindsWindow): 'CreateKeyBindsChildWindow' gets the parent 'WndClass' & modifies and registers a new 'WndClass' for the child window.

--- PgrPcClient/KeyBindsWindow.cs
+++ PgrPcClient/KeyBindsWindow.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Drawing;
-using Win32Api;
 using static Win32Api.Window;
 using static Win32Api.WinGdi;
 using static Win32Api.Macros;
@@ -12,22 +12,20 @@ namespace PgrPcClient
     {
         /*
          * TODO - Move CreateKeymapsHelperWindow to its own class
-         * TODO - GetClass of parentHWnd and modify lpfnWndProc
          * TODO - Fix Anti Aliasing of text
          * TODO - Be able to configure parameters of DrawString ( Font, Color, etc )
          */
-        public static nint CreateKeymapsHelperWindow(
+        public static nint CreateKeyBindsChildWindow(
             nint parentHWnd,
-            string appClassName,
             IEnumerable<string> keysToDraw,
             (int X, int Y) startPosition,
             int padding)
         {
             var cHWnd = CreateWindowExW(
                 0x00080000,
-                appClassName,
+                RegisterChildWndClass(parentHWnd),
                 null,
-                (uint)(Window.WS.VISIBLE | Window.WS.MAXIMIZE | Window.WS.POPUP),
+                (uint) (WS.VISIBLE | WS.MAXIMIZE | WS.POPUP),
                 0,
                 0,
                 0,
@@ -38,29 +36,80 @@ namespace PgrPcClient
                 IntPtr.Zero
             );
 
-            SetLayeredWindowAttributes(cHWnd, (uint)RGB(255, 255, 255), 255, 1);
+            SetLayeredWindowAttributes(cHWnd, (uint) RGB(255, 255, 255), 255, 1);
 
-            WinGdi.PAINTSTRUCT ps = new();
-            var hdc = BeginPaint(cHWnd, ps);
+            DrawKeyBinds(cHWnd, keysToDraw, startPosition, padding);
+            
+            return cHWnd;
 
-            using(var graphics = Graphics.FromHdc(hdc))
+            static string RegisterChildWndClass(nint parentHWnd)
             {
-                foreach(var key in keysToDraw)
-                {
-                    graphics.DrawString(
-                        key,
-                        new Font("Arial", 14),
-                        new SolidBrush(Color.Red),
-                        new PointF(startPosition.X, startPosition.Y)
-                    );
+                var parentWndClassEx = GetParentWindowClassInfo(parentHWnd);
 
-                    startPosition.X -= padding;
+                var childLpszClassName = $"child:{parentWndClassEx.lpszClassName}";
+
+                parentWndClassEx.lpszClassName = childLpszClassName;
+                parentWndClassEx.lpfnWndProc = DefWindowProc;
+
+                if(RegisterClassExA(ref parentWndClassEx) == 0)
+                {
+                    throw new Exception("RegisterClassExA failed.");
                 }
+
+                return childLpszClassName;
             }
 
-            EndPaint(hdc, ref ps);
+            static void DrawKeyBinds(
+                nint hWnd,
+                IEnumerable<string> keysToDraw,
+                (int X, int Y) startPosition,
+                int padding)
+            {
+                PAINTSTRUCT ps = new();
+                var hdc = BeginPaint(hWnd, ps);
 
-            return cHWnd;
+                using(var graphics = Graphics.FromHdc(hdc))
+                {
+                    foreach(var key in keysToDraw)
+                    {
+                        graphics.DrawString(
+                            key,
+                            new Font("Arial", 14),
+                            new SolidBrush(Color.Red),
+                            new PointF(startPosition.X, startPosition.Y)
+                        );
+
+                        startPosition.X -= padding;
+                    }
+                }
+
+                EndPaint(hdc, ref ps);
+            }
+
+            static WNDCLASSEX GetParentWindowClassInfo(nint parentHWnd)
+            {
+                const int maxLpszClassNameLength = 256;
+
+                var arrayPool = ArrayPool<char>.Shared;
+
+                var sharedArray = arrayPool.Rent(maxLpszClassNameLength);
+
+                if(GetClassName(parentHWnd, sharedArray, maxLpszClassNameLength) == 0)
+                {
+                    throw new Exception("GetClassName failed.");
+                }
+
+                var lpszClassName = string.Concat(sharedArray);
+
+                arrayPool.Return(sharedArray);
+
+                if(!GetClassInfoExW(0, lpszClassName, out var lpwcz))
+                {
+                    throw new Exception("GetClassInfoExW failed.");
+                }
+
+                return lpwcz;
+            }
         }
     }
 }
