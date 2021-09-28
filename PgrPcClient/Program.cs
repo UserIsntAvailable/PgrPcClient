e2a7515fa01bbd71f334d70b8381ac192855232a
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-09-28 00:30:41 -0400

    Change(Program): Re-implement key binds child window.

--- PgrPcClient/Program.cs
+++ PgrPcClient/Program.cs
@@ -4,17 +4,17 @@ using System.Net;
 using AdbMouseFaker;
 using Microsoft.Extensions.Configuration;
 using PgrPcClient;
+using PgrPcClient.Extensions;
 using SharpAdbClient;
 using Win32Api;
 using WindowsAppOverlay;
 using static Win32Api.Mouse;
 using static Win32Api.Window;
+using static Win32Api.Message;
 using ConfigurationParser = PgrPcClient.ConfigurationParser;
 
 /*
- * TODO - Find and parse IN-GAME config files of PGR. 
  * TODO - Implement auto reloading of the appsettings.json
- * TODO - Create a better 'background service'
  * TODO - Create an ADB server if not started
  */
 while(true)
@@ -47,6 +47,47 @@ while(true)
         );
 
         MessageHandler messageHandler = new();
+        messageHandler.Map(
+            WM.CREATE,
+            // TODO - Refactor ( I will parse the IN-GAME config file directly )
+            (hWnd, _, _) =>
+            {
+                var keys = config.GetSection("OverlayInfo:Bindings")
+                                 .GetChildren()
+                                 .Where(child => child.Value.StartsWith("Signal"))
+                                 .OrderBy(child => child.Value)
+                                 .Select(child =>
+                                     // TODO - Create string parser
+                                     {
+                                         var value = child.Key;
+
+                                         if(value.Length == 1) return value;
+
+                                         if(value.IsHexValue())
+                                         {
+                                             return value == "0x0E" ? "WU" : "WD";
+                                         }
+
+                                         if(value.StartsWith("XB"))
+                                         {
+                                             return$"XB{value[^1]}";
+                                         }
+
+                                         return value[..2];
+                                     }
+                                 );
+
+                KeyBindsWindow.CreateKeyBindsChildWindow(
+                    hWnd,
+                    keys,
+                    (int.Parse(config["KeyBindsChildWindowSettings:xStartPosition"]),
+                     int.Parse(config["KeyBindsChildWindowSettings:yStartPosition"])), 
+                    int.Parse(config["KeyBindsChildWindowSettings:Padding"])
+                );
+
+                return 0;
+            }
+        );
         // I'm not sure if I wanna keep this as a 'class'; there should be a better alternative. 
         new HandleMessageMapper(messageHandler, messageFaker, mouseFaker);
         AppOverlay overlay = new(messageHandler, config["OverlayInfo:AppClassName"]);
@@ -57,7 +98,7 @@ while(true)
 static IConfiguration SetupConfig(nint pgrHandle)
 {
     return new ConfigurationBuilder().AddJsonFile("appsettings.json").AddInMemoryCollection(
-        new[] {new KeyValuePair<string, string>("PgrAppHWnd", pgrHandle.ToString()),}
+        new[] { new KeyValuePair<string, string>("PgrAppHWnd", pgrHandle.ToString()), }
     ).Build();
 }
 
